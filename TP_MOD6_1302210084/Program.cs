00133f4cd379706d1ab89a535aeddc88cb70b612
SuryaaAulia <suryaaulia489@gmail.com>
2023-03-26 23:32:12 +0700

    implementasi design by contract

--- TP_MOD6_1302210084/Program.cs
+++ TP_MOD6_1302210084/Program.cs
@@ -1,6 +1,13 @@
 ﻿using System.Diagnostics.Contracts;
 using TP_MOD6_1302210084;
 
-SayaTubeVideo yt = new SayaTubeVideo("Tutorial Design By Contract – [SURYA AULIA]");
-yt.IncreasePlayCount(450000);
-yt.PrintVideoDetails();
+SayaTubeVideo obj1 = new SayaTubeVideo("Tutorial Design By Contract – [SURYA AULIA]");
+obj1.IncreasePlayCount(500000);
+obj1.PrintVideoDetails();
+
+SayaTubeVideo obj2 = new SayaTubeVideo("FaZe vs Cloud9 - ESL Pro League Season 17 - Grand Final");
+for (int i = 0; i < 500; i++)
+{
+    obj2.IncreasePlayCount(10000000);
+}
+obj2.PrintVideoDetails();
--- TP_MOD6_1302210084/SayaTubeVideo.cs
+++ TP_MOD6_1302210084/SayaTubeVideo.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Diagnostics.Metrics;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,18 +18,35 @@ namespace TP_MOD6_1302210084
 
         public SayaTubeVideo(String title)
         {
+            Debug.Assert(title != null && title.Length <= 100, "judul yang dimasukan lebih dari 100 karakter atau kosong");
             Random rnd = new Random();
-            this.title = title;
-            this.playCount = 0;
-            this.id = rnd.Next(10000, 100000);
+            try
+            {
+                this.title = checked(title);
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            playCount = 0;
+            id = rnd.Next(10000, 100000);
         }
-        public void IncreasePlayCount(int count)
+        public void IncreasePlayCount(int playCount)
         {
-            this.playCount += count;
+            Debug.Assert(playCount < 10000000);
+
+            try
+            {
+                this.playCount = checked(this.playCount + playCount);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
+
         public void PrintVideoDetails()
         {
-            Console.WriteLine("Id video "+this.id+" dengan judul "+this.title+" memiliki jumlah viewer "+ this.playCount);
+            Console.WriteLine("Id video "+this.id+" dengan judul "+this.title+" memiliki jumlah viewers "+ this.playCount);
         }
     }
 }
