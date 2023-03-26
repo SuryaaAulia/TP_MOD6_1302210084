3973ae4914eeef2b8d6ffe3ea71c5ded1bfa6b2d
unknown <you@example.com>
2023-03-26 11:46:22 +0700

    menambahkan class SayaTubeVideo

--- TP_MOD6_1302210084/Program.cs
+++ TP_MOD6_1302210084/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics.Contracts;
+using TP_MOD6_1302210084;
+
+SayaTubeVideo yt = new SayaTubeVideo("Tutorial Design By Contract – [SURYA AULIA]");
+yt.IncreasePlayCount(450000);
+yt.PrintVideoDetails();
---
+++ TP_MOD6_1302210084/SayaTubeVideo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_MOD6_1302210084
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(String title)
+        {
+            Random rnd = new Random();
+            this.title = title;
+            this.playCount = 0;
+            this.id = rnd.Next(10000, 100000);
+        }
+        public void IncreasePlayCount(int count)
+        {
+            this.playCount += count;
+        }
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("Id video "+this.id+" dengan judul "+this.title+" memiliki jumlah viewer "+ this.playCount);
+        }
+    }
+}
